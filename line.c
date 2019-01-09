110a526ac8f69a4fb0340702243b712d8c6b1ee2
Martin DE LAGARDE <mde-laga@e2r12p15.42.fr>
2019-01-09 17:02:33 +0100

    verifline

--- line.c
+++ line.c
@@ -6,11 +6,13 @@
 /*   By: mde-laga <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 13:43:52 by mde-laga          #+#    #+#             */
-/*   Updated: 2019/01/08 17:58:35 by mde-laga         ###   ########.fr       */
+/*   Updated: 2019/01/09 17:02:17 by mde-laga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+#include <stdio.h>
+#include <fcntl.h>
 
 char	*ft_getline(int fd, char *str)
 {
@@ -32,73 +34,71 @@ char	*ft_getline(int fd, char *str)
 }
 
 /*char	**ft_cutline(char *str)
-  {
-  char	**tab;
-  int		i;
-  int		j;
-  int		k;
-  int		size;
-
-  i = 0;
-  j = 0;
-  k = 0;
-  size = 0;
-  while (str[j])
-  {
-  if (str[j] == '\n' && (str[j + 1] == '\n' || !str[j + 1]))
-  size++;
-  j++;
-  }
-  dprintf(1, "2");
-  if (!(tab = (char**)malloc(sizeof(char*) * (size + 1))))
-  return (NULL);
-  j = 0;
-  while (str[j])
-  {
-  while (str[k] != '\n' && str[k + 1] != '\n' && str[k])
-  k++;
-  printf("k=%d\n", k);
-  tab[i] = ft_strsub(str, j, k + 1);
-  j = k;
-  printf("j=%d\n", j);
-  k++;
-  i++;
-  }
-  return (tab);
-  }*/
-
-#include <stdio.h>
-
-/*int		ft_check_neighbours(char *str)
 {
-	int i;
-	int count;
-	int check;
+	char	**tab;
+	int		i;
+	int		j;
+	int		k;
+	int		size;
 
 	i = 0;
-	count = 0;
-	while (str[i])
+	j = 0;
+	k = 0;
+	size = 0;
+	while (str[j])
 	{
-		if  (str[i] == '#')
-		{
-			if (str[i + 1] == '#')
-				count++;
-			if (str[i - 1] == '#')
-				count++;
-			if (str[i + 5] == '#')
-				count++;
-			if (str[i - 5] == '#')
-				count++;
-			check++;
-		}
+		if (str[j] == '\n' && (str[j + 1] == '\n' || !str[j + 1]))
+			size++;
+		j++;
+	}
+	dprintf(1, "2");
+	if (!(tab = (char**)malloc(sizeof(char*) * (size + 1))))
+		return (NULL);
+	j = 0;
+	while (str[j])
+	{
+		while (str[k] != '\n' && str[k + 1] != '\n' && str[k])
+			k++;
+		printf("k=%d\n", k);
+		tab[i] = ft_strsub(str, j, k + 1);
+		j = k;
+		printf("j=%d\n", j);
+		k++;
 		i++;
 	}
-	printf("%d\n", count);
-	if (check % 4 != 0)
-		return (1);
-	//	if (count == 6 || count == 8)
-	return (0);
-	return (1);
+	return (tab);
+}*/
+
+/*int		ft_check_neighbours(char *str)
+  {
+  int i;
+  int count;
+  int check;
+
+  i = 0;
+  count = 0;
+  while (str[i])
+  {
+  if  (str[i] == '#')
+  {
+  if (str[i + 1] == '#')
+  count++;
+  if (str[i - 1] == '#')
+  count++;
+  if (str[i + 5] == '#')
+  count++;
+  if (str[i - 5] == '#')
+  count++;
+  check++;
+  }
+  i++;
+  }
+  printf("%d\n", count);
+  if (check % 4 != 0)
+  return (1);
+//	if (count == 6 || count == 8)
+return (0);
+return (1);
 }*/
 
 int		ft_verifline(char *str)
@@ -107,19 +107,22 @@ int		ft_verifline(char *str)
 	int j;
 	int k;
 	int len;
+	int hash;
 
 	i = 0;
 	j = 1;
 	k = 0;
+	hash = 0;
 	len = ft_strlen(str);
 	if (len > 545 || str[i] == '\n' || str[len - 2] == '\n')
 		return (1);
 	while (str[i])
 	{
+		str[i] == '#' ? hash++ : hash;
 		if (str[i] == '\n' && str[i + 1] != '\n')
 			j++;
 		else if ((str[i] == '\n' && str[i + 1] == '\n') || !str[i + 2])
-			if (j != 4 || (j = 0))
+			if (hash != 4 || j != 4 || (j = 0) || (hash = 0))
 				return (1);
 		if (str[i] == '\n' && str[i - 1] == '\n' && str[i + 1] == '\n')
 			return (1);
@@ -131,8 +134,6 @@ int		ft_verifline(char *str)
 	}
 	return (0);
 }
-
-#include <fcntl.h>
 
 int		main(int ac, char **av)
 {
@@ -146,9 +147,9 @@ int		main(int ac, char **av)
 		return (0);
 	str = ft_getline(fd, str);
 	if (ft_verifline(str) == 0)
-		puts(str);
+		ft_putstr(str);
 	else
-		puts("error");
+		ft_putstr("error");
 	free(str);
 	return (0);
 }
